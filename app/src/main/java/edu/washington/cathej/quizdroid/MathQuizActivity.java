2fdc7c4a22b402be85f32939cc041a7474b6c061
Cat <cathej6@gmail.com>
2017-02-01 15:13:32 -0800

    Submitting completed base assignment.

--- app/src/main/java/edu/washington/cathej/quizdroid/MathQuizActivity.java
+++ app/src/main/java/edu/washington/cathej/quizdroid/MathQuizActivity.java
@@ -1,26 +1,44 @@
 package edu.washington.cathej.quizdroid;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.app.Fragment;
 
+import static android.R.attr.fragment;
+import static edu.washington.cathej.quizdroid.R.id.finish;
+import static edu.washington.cathej.quizdroid.R.id.next;
 import static edu.washington.cathej.quizdroid.R.id.options;
 import static edu.washington.cathej.quizdroid.R.id.question;
+import static edu.washington.cathej.quizdroid.R.id.submit;
+import static edu.washington.cathej.quizdroid.R.id.total;
+import static edu.washington.cathej.quizdroid.R.id.yanswer;
 
 public class MathQuizActivity extends Activity {
 
     private String[] questions;
     private String[] answers;
     private int[] correctAnswerIndex;
+    private String[] stages = new String[] {"overview", "question", "answer"};
+
+    private String status = stages[0];
 
     private int questionNum = 0;
     private int correctAnswers = 0;
+
+    private OverviewFragment overviewFragment;
+    private QuestionFragment questionFragment;
+    private AnswerFragment answerFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,101 +52,75 @@ public class MathQuizActivity extends Activity {
         answers = i.getStringArrayExtra("answers");
         correctAnswerIndex = i.getIntArrayExtra("correctAnswers");
 
+        Bundle bOverview = new Bundle();
+        bOverview.putString("quizName", quizName);
+        bOverview.putInt("count", questions.length);
+        overviewFragment = new OverviewFragment();
+        overviewFragment.setArguments(bOverview);
 
-        TextView name = (TextView) findViewById(R.id.name);
-        name.setText(quizName);
-        TextView description = (TextView) findViewById(R.id.description);
-        description.setText("This topic centers around the magical world of " + quizName
-                + " and the wonderous things you can learn from it");
-        TextView count = (TextView) findViewById(R.id.count);
-        count.setText("Number of questions: " + questions.length);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.overview, overviewFragment);
+        ft.commit();
 
-
-        final LinearLayout questionPage = (LinearLayout) findViewById(R.id.questionpage);
-        final LinearLayout answerPage = (LinearLayout) findViewById(R.id.answerpage);
-
-        final TextView yAnswer = (TextView) findViewById(R.id.yanswer);
-        final TextView mAnswer = (TextView) findViewById(R.id.manswer);
-        final TextView total = (TextView) findViewById(R.id.total);
-        final TextView question = (TextView) findViewById(R.id.question);
-        question.setText(questions[0]);
+        final Button button = (Button) findViewById(R.id.button);
 
 
-        final RadioGroup options = (RadioGroup) findViewById(R.id.options);
-        createRadioButtons(options);
-
-        final Button submit = (Button) findViewById(R.id.submit);
-        final Button next = (Button) findViewById(R.id.next);
-        final Button finish = (Button) findViewById(R.id.finish);
-
-        Button begin = (Button) findViewById(R.id.begin);
-        begin.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LinearLayout overview = (LinearLayout) findViewById(R.id.overview);
-                overview.setVisibility(View.GONE);
-                submit.setVisibility(View.VISIBLE);
-                questionPage.setVisibility(View.VISIBLE);
-            }
-        });
+                if (status == stages[0] || (status == stages[2] && button.getText() == "Next")) {
+                    Bundle bQuestion = new Bundle();
+                    bQuestion.putStringArray("questions", questions);
+                    bQuestion.putStringArray("answers", answers);
+                    bQuestion.putInt("questionNum", questionNum);
 
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (options.getCheckedRadioButtonId() != -1) {
-                    questionPage.setVisibility(View.GONE);
-                    submit.setVisibility(View.GONE);
-                    answerPage.setVisibility(View.VISIBLE);
+                    questionFragment = new QuestionFragment();
+                    questionFragment.setArguments(bQuestion);
+
+                    FragmentTransaction tx = getFragmentManager().beginTransaction();
+                    tx.replace(R.id.overview, questionFragment);
+                    tx.commit();
+                    status = stages[1];
+                    button.setText("Submit");
+                } else if (status == stages[1] &&
+                        questionFragment.options.getCheckedRadioButtonId() != -1) {
                     if (questionNum == questions.length - 1) {
-                        finish.setVisibility(View.VISIBLE);
+                        button.setText("Finish");
                     } else {
-                        next.setVisibility(View.VISIBLE);
+                        button.setText("Next");
                     }
-                    int selectedId = options.getCheckedRadioButtonId();
-                    RadioButton theirAnswer = (RadioButton) options.findViewById(selectedId);
-                    int answerIndex = options.indexOfChild(theirAnswer);
+                    int selectedId = questionFragment.options.getCheckedRadioButtonId();
+                    RadioButton theirAnswer = (RadioButton) questionFragment.options.findViewById(selectedId);
+                    int answerIndex = questionFragment.options.indexOfChild(theirAnswer);
                     if (answerIndex == correctAnswerIndex[questionNum]) {
                         correctAnswers++;
                     }
 
-                    yAnswer.setText("Your answer: " + answers[questionNum * 4 + answerIndex]);
-                    mAnswer.setText("My answer: " + answers[questionNum * 4 + correctAnswerIndex[questionNum]]);
+                    String yAnswer = "Your answer: " + answers[questionNum * 4 + answerIndex];
+                    String mAnswer = "My answer: " + answers[questionNum * 4 +
+                            correctAnswerIndex[questionNum]];
                     questionNum++;
-                    total.setText("You have " + correctAnswers + " out of " + questionNum
-                            + " correct");
+                    String total = "You have " + correctAnswers + " out of " + questionNum
+                            + " correct";
+
+                    Bundle bAnswer = new Bundle();
+                    bAnswer.putString("yAnswer", yAnswer);
+                    bAnswer.putString("mAnswer", mAnswer);
+                    bAnswer.putString("total", total);
+
+                    final AnswerFragment answerFragment = new AnswerFragment();
+                    answerFragment.setArguments(bAnswer);
+
+                    FragmentTransaction tx = getFragmentManager().beginTransaction();
+                    tx.replace(R.id.overview, answerFragment);
+                    tx.commit();
+                    status = stages[2];
+
+                } else if (status == stages[2] && button.getText() == "Finish") {
+                    Intent intent = new Intent(v.getContext(), MainActivity.class);
+                    v.getContext().startActivity(intent);
                 }
             }
         });
-
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (options.getCheckedRadioButtonId() != -1) {
-                    options.removeAllViews();
-                    createRadioButtons(options);
-                    question.setText(questions[questionNum]);
-                    questionPage.setVisibility(View.VISIBLE);
-                    submit.setVisibility(View.VISIBLE);
-                    answerPage.setVisibility(View.GONE);
-                    next.setVisibility(View.GONE);
-                }
-            }
-        });
-
-        finish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), MainActivity.class);
-                v.getContext().startActivity(intent);
-            }
-        });
-    }
-
-    public void createRadioButtons(RadioGroup options) {
-        for (int i = 0; i < 4; i++) {
-            RadioButton option = new RadioButton(this);
-            option.setText(answers[questionNum * 4 + i]);
-            options.addView(option);
-        }
     }
 }
--- app/src/main/java/edu/washington/cathej/quizdroid/OverviewFragment.java
+++ app/src/main/java/edu/washington/cathej/quizdroid/OverviewFragment.java
@@ -3,21 +3,24 @@ package edu.washington.cathej.quizdroid;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link TestFragment.OnFragmentInteractionListener} interface
+ * {@link OverviewFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link TestFragment#newInstance} factory method to
+ * Use the {@link OverviewFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TestFragment extends Fragment {
+public class OverviewFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +32,14 @@ public class TestFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public TestFragment() {
+    public View view;
+
+    public TextView name;
+    public TextView description;
+    public TextView count;
+    public Button begin;
+
+    public OverviewFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +49,11 @@ public class TestFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TestFragment.
+     * @return A new instance of fragment OverviewFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TestFragment newInstance(String param1, String param2) {
-        TestFragment fragment = new TestFragment();
+    public static OverviewFragment newInstance(String param1, String param2) {
+        OverviewFragment fragment = new OverviewFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +74,20 @@ public class TestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        view = inflater.inflate(R.layout.fragment_overview, container, false);
+
+        name = (TextView) view.findViewById(R.id.name);
+        name.setText(getArguments().getString("quizName"));
+        description = (TextView) view.findViewById(R.id.description);
+        description.setText("This topic centers around the magical world of " +
+                getArguments().getString("quizName") +
+                " and the wonderous things you can learn from it");
+        count = (TextView) view.findViewById(R.id.count);
+        count.setText("Number of questions: " + getArguments().getInt("count"));
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -105,4 +128,5 @@ public class TestFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
 }
